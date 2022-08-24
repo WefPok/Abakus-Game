314a7d71d384213799fcc316e1339991cf58a41a
WefPok <46328702+WefPok@users.noreply.github.com>
2022-08-24 21:10:17 +0300

    Full rework

--- hand_recognizer_class.py
+++ hand_recognizer_class.py
@@ -1,3 +1,6 @@
+import math
+import time
+
 import cv2
 from tensorflow.python.keras.models import load_model
 import mediapipe as mp
@@ -10,9 +13,10 @@ model = load_model('mp_hand_gesture')
 
 
 class EventStack:
-    def __init__(self, length, placeholder="PlaceHolder"):
+    def __init__(self, length, placeholder=2):
         self.stack = [placeholder] * length
         self.length = length
+        self.placeholder = placeholder
 
     def append(self, event):
         self.stack.pop(0)
@@ -25,11 +29,10 @@ class EventStack:
         return self.stack
 
     def clear(self):
-        self.stack = ["PlaceHolder"] * self.length
+        self.stack = [self.placeholder] * self.length
 
 
 def get_x_range(wrist_x):
-    res = 9
     if wrist_x < 0.1:
         res = 0
     elif 0.1 <= wrist_x < 0.2:
@@ -53,124 +56,187 @@ def get_x_range(wrist_x):
     return res
 
 
-def create_json(hand_lanmark1, hand_lanmark2, analyze_res1, analyze_res2):
-    res = {}
-    wrist_x = hand_lanmark1[0].x
-    wrist_x = get_x_range(wrist_x)
+# def create_json(hand_lanmark1, hand_lanmark2, analyze_res1, analyze_res2):
+#     res = {}
+#     wrist_x = hand_lanmark1[0].x
+#     wrist_x = get_x_range(wrist_x)
+#
+#     res['right_hand'] = {"x_position": wrist_x,
+#                          "events": analyze_res1}
+#
+#     wrist_x = hand_lanmark2[0].x
+#     wrist_x = get_x_range(wrist_x)
+#
+#     res['left_hand'] = {"x_position": wrist_x,
+#                         "events": analyze_res2}
+#
+#     return json.dumps(res)
 
-    res['right_hand'] = {"x_position": wrist_x,
-                         "events": analyze_res1}
+class Hand:
+    def __init__(self, hand_id):
+        self.hand_id = hand_id
+        if self.hand_id == 0:
+            self.name = "left"
+        else:
+            self.name = "right"
+        self.thumb_plain_stack = EventStack(20, 0)
+        self.index_plain_stack = EventStack(20, 0)
 
-    wrist_x = hand_lanmark2[0].x
-    wrist_x = get_x_range(wrist_x)
+        self.poi = None
 
-    res['left_hand'] = {"x_position": wrist_x,
-                        "events": analyze_res2}
+        self.path_total_index = EventStack(10, 0)
+        self.angle_index = EventStack(10, 0)
 
-    return json.dumps(res)
+        self.path_total_thumb = EventStack(10, 0)
+        self.angle_thumb = EventStack(10, 0)
+        self.x_pos = 0
+
+        self.index_bent = False
+        self.index_moved = 0
+        self.index_timeout = 0
+
+        self.thumb_bent = False
+        self.thumb_timeout = 0
+        # self.thumb_moved = False
+
+    def update_hand_position(self, points):
+        self.x_pos = get_x_range(points[-1].x)
+        y_pos = points[-1].y
+
+        self.thumb_plain_stack.append(points[0].y - y_pos)
+        self.index_plain_stack.append(points[3].y - y_pos)
+
+        self.poi = points
+
+        index_plain = self.index_plain_stack.get_stack()
+        self.path_total_index.append(int(1000 * sum(
+            [abs(i - j) for i, j in zip(index_plain[1:], index_plain[2:-1])])))
+
+        thumb_plain = self.thumb_plain_stack.get_stack()
+        self.path_total_thumb.append(int(1000 * sum(
+            [abs(i - j) for i, j in zip(thumb_plain[1:], thumb_plain[2:-1])])))
+
+        ip2, ip1, ip3 = self.poi[3:6]
+        self.angle_index.append(abs(
+            180 - abs(int(math.degrees(
+                math.atan2(ip3.y - ip1.y, ip3.x - ip1.x) - math.atan2(ip2.y - ip1.y, ip2.x - ip1.x))))))
+
+        tp2, tp1, tp3 = self.poi[0:3]
+        self.angle_thumb.append(abs(
+            180 - abs(int(math.degrees(
+                math.atan2(tp3.y - tp1.y, tp3.x - tp1.x) - math.atan2(tp2.y - tp1.y, tp2.x - tp1.x))))))
+
+    def set_state_variables(self):
+        events = {"+1": False,
+                  "-1": False,
+                  "+5": False,
+                  "-5": False}
+        if self.thumb_timeout == 0:
+            thumb_first5 = self.angle_thumb.get_stack()[0:5]
+            if all(i < 50 for i in thumb_first5):
+                if self.thumb_bent:
+                    self.thumb_bent = False
+            else:
+                if not self.thumb_bent:
+                    print("+1 Thumb " + self.name)
+                    events["+1"] = True
+                    self.thumb_bent = True
+                    self.thumb_timeout = 5
+                    self.angle_thumb.clear()
+        else:
+            self.thumb_timeout -= 1
+
+        if self.index_timeout == 0:
+            index_first5 = self.angle_index.get_stack()[0:5]
+            if all(i < 40 for i in index_first5):
+                if self.index_bent:
+                    self.index_bent = False
+                if sum(self.path_total_index.get_stack()) > 1200:
+                    if not self.index_moved:
+                        print("-1 Index " + self.name)
+                        events["-1"] = True
+                        self.index_moved = True
+                        self.index_timeout = 10
+                elif self.index_moved:
+                    self.index_moved = False
+            else:
+                if not self.index_bent and not self.index_moved:
+                    self.index_bent = True
+                    self.index_moved = True
+                    self.index_timeout = 10
+                    if sum(self.path_total_index.get_stack()) > 800:
+                        events["-5"] = True
+                        print("-5 Index " + self.name)
+                    else:
+                        events["+5"] = True
+                        print("+5 Index " + self.name)
+        else:
+            self.index_timeout -= 1
+        return events
 
 
-class Recognizer:
+class Analyzer:
     def __init__(self):
-        self.thumb_stack1 = EventStack(5)
-        self.thumb_stack2 = EventStack(5)
-        self.thumb_state1 = True
-        self.thumb_state2 = True
+        self.hands = [Hand(0), Hand(1)]
 
-        self.index_stack1 = EventStack(5)
-        self.index_stack2 = EventStack(5)
-        self.index_state1 = True
-        self.index_state2 = True
-
-    def recognize(self, frame):
+    def update_hands(self, frame, show=True):
         framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         result = hands.process(framergb)
 
         if result.multi_hand_landmarks and len(result.multi_hand_landmarks) == 2:
+            hand1 = result.multi_hand_landmarks[0].landmark
+            hand2 = result.multi_hand_landmarks[1].landmark
 
-            hand1 = result.multi_hand_landmarks[0]
-            hand2 = result.multi_hand_landmarks[1]
+            points1 = [hand1[4], hand1[3], hand1[1], hand1[8], hand1[6], hand1[5], hand1[0]]
+            points2 = [hand2[4], hand2[3], hand2[1], hand2[8], hand2[6], hand2[5], hand2[0]]
 
-            analysis1 = self.event_analyzer(self.thumb_stack1, self.index_stack1, hand1.landmark, self.thumb_state1,
-                                            self.index_state1, 1)
-            analysis2 = self.event_analyzer(self.thumb_stack2, self.index_stack2, hand2.landmark, self.thumb_state2,
-                                            self.index_state2, 2)
+            if hand1[0].x < hand2[0].x:
+                self.hands[0].update_hand_position(points1)
+                self.hands[1].update_hand_position(points2)
+            else:
+                self.hands[0].update_hand_position(points2)
+                self.hands[1].update_hand_position(points1)
+            if show:
+                h, w = frame.shape[:2]
+                copy = frame.copy()
+                for point in self.hands[0].poi:
+                    x = int(point.x * w)
+                    y = int(point.y * h)
+                    copy = cv2.circle(copy, (x, y), 2, (0, 255, 0), -1)
+                for point in self.hands[1].poi:
+                    x = int(point.x * w)
+                    y = int(point.y * h)
+                    copy = cv2.circle(copy, (x, y), 2, (255, 0, 0), -1)
 
-            json = create_json(hand1.landmark, hand2.landmark, analysis1, analysis2)
+                cv2.imshow("handlandmarks", copy)
 
-            return json
-        else:
-            return None
+    def set_no_events(self, hand: Hand):
+        for key in self.event_json[hand.hand_id].keys():
+            self.event_json[hand.hand_id][key] = False
 
-    def event_analyzer(self, thumb_pos: EventStack, index_pos: EventStack, hand_landmark, thumb_state, index_state,
-                       hand_n):
-        wrist_position = hand_landmark[0].y
-        thumb_position = hand_landmark[4].y - wrist_position
-        index_position = hand_landmark[8].y - wrist_position
+        # def analyze_hand_thumb(self, hand: Hand):
 
-        thumb_pos.append(thumb_position)
-        index_pos.append(index_position)
+    #     hand_thumb_stack = hand.thumb_plain_stack.get_stack()
+    #     if 2 in hand_thumb_stack:
+    #         self.set_no_events(hand)
+    #     total_distance = [hand_thumb_stack[n] - hand_thumb_stack[n - 1] for n in range(1, len(hand_thumb_stack))]
+    #     print(total_distance)
+    def process_hand_position(self):
+        for hand in self.hands:
+            hand.set_state_variables()
 
-        res = {"+5": False,
-               "+1": False,
-               "-5": False,
-               "-1": False}
+    def main(self, frame):
 
-        if "PlaceHolder" not in thumb_pos.get_stack():
-            diff_thumb = thumb_position - thumb_pos.get_stack()[-3]
 
-            if abs(diff_thumb) > 0.05 and diff_thumb > 0 and thumb_state:
-                res["+1"] = True
-                print("Op: +1")
-                if hand_n == 1:
-                    self.thumb_state1 = False
-                    self.thumb_stack1.clear()
-                else:
-                    self.thumb_state2 = False
-                    self.thumb_stack2.clear()
-            elif not thumb_state:
-                if hand_n == 1:
-                    self.thumb_state1 = True
-                else:
-                    self.thumb_state2 = True
-
-        if "PlaceHolder" not in index_pos.get_stack():
-            diff_index = index_position - index_pos.get_stack()[-3]
-            if 0.05 < abs(diff_index) and diff_index < 0 and index_state:
-                res["+5"] = True
-                print("Op: +5")
-                if hand_n == 1:
-                    self.index_state1 = False
-                    self.index_stack1.clear()
-                else:
-                    self.index_state2 = False
-                    self.index_stack2.clear()
-
-            elif abs(diff_index) > 0.7 and diff_index > 0 and index_state:
-                res["-1"] = True
-                print("Op: -1")
-                if hand_n == 1:
-                    self.index_state1 = False
-                    self.index_stack1.clear()
-                else:
-                    self.index_state2 = False
-                    self.index_stack2.clear()
-
-            elif 0.05 < abs(diff_index) < 0.7 and diff_index > 0 and index_state:
-                res["-5"] = True
-                print("Op: -5")
-                if hand_n == 1:
-                    self.index_state1 = False
-                    self.index_stack1.clear()
-                else:
-                    self.index_state2 = False
-                    self.index_stack2.clear()
+        self.update_hands(frame)
+        # print(self.hands[0].angle_index.get_stack())
+        self.process_hand_position()
+        event_json = {"right_hand": {"x_position": self.hands[0].x_pos,
+                                     "events": self.hands[0].set_state_variables()},
 
 
 
-            elif not index_state:
-                if hand_n == 1:
-                    self.index_state1 = True
-                else:
-                    self.index_state2 = True
-        return res
+                      "left_hand": {"x_position": self.hands[1].x_pos,
+                                     "events": self.hands[1].set_state_variables()}}
+
+        return event_json
--- server_test.py
+++ server_test.py
@@ -1,21 +1,22 @@
 import asyncio
 import websockets
 import cv2
-from hand_recognizer_class import Recognizer
+from hand_recognizer_class import Analyzer
 
 cap = cv2.VideoCapture(0)
 
-rec = Recognizer()
+rec = Analyzer()
 
 
 async def handler(websocket):
     while True:
         ret, frame = cap.read()
         await asyncio.sleep(0.01)
-        res = rec.recognize(frame)
+        res = rec.main(frame)
         if res is not None:
             await websocket.send(res)
-
+        if cv2.waitKey(1) == 1:
+            break
 
 async def main():
     async with websockets.serve(handler, "", 8001):
